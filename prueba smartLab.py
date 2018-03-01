15594acbf26bae9a89990a59fd60b2f0a940814b
Luis Sustaita <ld.delacruzsustaita@ugto.mx>
2018-03-01 15:28:00 -0600

    Se corrigieron errores de codigo. Hay que actualizar el token si no funciona

--- prueba smartLab.py
+++ prueba smartLab.py
@@ -11,7 +11,7 @@ COOKIE='datr=WOy-WfHNoH9xZxDWb_h499W7; sb=WOy-WR642QFFfyPxCRfeOoEr; c_user=10000
 FB_DTSG='AQF3FM3TklNW:AQF_ijFzbZww'
 
 # Insert your image file path here    
-photo='amigos.jpg'
+photo='holis.jpg'
 
 #Open the image
 img=Image.open(photo)
@@ -25,31 +25,29 @@ font=ImageFont.truetype('arial.ttf',20)
 
 w,h=img.size #Obtenemos las dimensiones de la foto para despues redimensionar las medidas obtenidas del wrapper
 
-
-def wrapper(photo,access_token,cookie,fb_dtsg):
-    #An unofficial python wrapper for the Facebook face recognition endpoint
-    from fbrecog import FBRecog
+#An unofficial python wrapper for the Facebook face recognition endpoint
+from fbrecog import FBRecog
     
-    # Instantiate the recog class
-    recog = FBRecog(access_token, cookie, fb_dtsg)
+# Instantiate the recog class
+recog = FBRecog(TOKEN, COOKIE, FB_DTSG)
 
-    # Recog class can be used multiple times with different paths
-    #print(recog.recognize(photo))    
+# Recog class can be used multiple times with different paths
+#print(recog.recognize(photo))    
 
-    #print('-'*40)
-    # Call recognize_raw to get more info about the faces detected, including their positions
-    #print(recog.recognize_raw(photo))
+#print('-'*40)
+# Call recognize_raw to get more info about the faces detected, including their positions
+#print(recog.recognize_raw(photo))
 
-    faces = recog.recognize_raw(path)
-    result=[]
-    for face in faces:
-        name=face['recognitions']    
-        if name:
-            print('name: '+name[0]['user']['name'])
-            posx=face['x']*w/100        
-            posy=face['y']*h/100            
-            draw.text((posx,posy),name[0]['user']['name'],fill='blue',font=font)            
-            draw.rectangle(((posx+(face['width']*w/100)/2,posy+(face['height']*h/100)/2),(posx-(face['width']*w/100)/2,posy-(face['height']*w/100)/2)),fill=None,outline='red')
+faces = recog.recognize_raw(photo)
+result=[]
+for face in faces:
+    name=face['recognitions']    
+    if name:
+        print('name: '+name[0]['user']['name'])
+        posx=face['x']*w/100        
+        posy=face['y']*h/100
+        size=face['width']*face['height']        
+        draw.text((posx,posy),name[0]['user']['name'],fill='blue',font=font)        
+        draw.rectangle(((posx+(face['width']*w/100)/2,posy+(face['height']*h/100)/2),(posx-(face['width']*w/100)/2,posy-(face['height']*w/100)/2)),fill=None,outline='red')
 
-    plt.imshow(background)
-    img.save('reconocidos.jpg')
+img.save('reconocidos.jpg')
