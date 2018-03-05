11837863b6c3b2ff55d360faec573fc28153eed0
Luis Sustaita <ld.delacruzsustaita@ugto.mx>
2018-03-05 13:10:15 -0600

    se agrego manejo de excepciones

--- control-access.py
+++ control-access.py
@@ -8,7 +8,7 @@ flag=True
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
-while(True):
+while(flag):
    #leemos un frame y lo guardamos
    ret, img = cap.read()
 
@@ -26,23 +26,25 @@ while(True):
        break
    
    #Mostramos la imagen
-   cv2.imshow('img',img)   
+   #cv2.imshow('img',img)   
    
    #con la tecla 'q' salimos del programa
    #if cv2.waitKey(1) & 0xFF == ord('q'):
    #    break
       
-   if i>5: #esperamos a que se estabilice la imagen para guardar una fotografia
+   if i>5: #esperamos a que se estabilice o aparezca una cara visible en la imagen para guardar una fotografia
       cv2.imwrite('save.jpg', img)
-      flag=False      
-   #detenemos el bucle para buscar caras
-   #break
+      #detenemos el bucle para buscar caras
+      flag=False         
 
 cap.release()
 cv2.destroyAllWindows()
 
 
 #INICIO DE FBRECOG PARA RECONOCIMIENTO FACIAL
+#An unofficial python wrapper for the Facebook face recognition endpoint
+from fbrecog import FBRecog
+
 #Para dibujar rectangulos y nombres sobre las caras
 from PIL import Image, ImageDraw, ImageFont
 
@@ -69,9 +71,6 @@ draw=ImageDraw.Draw(img)
 font=ImageFont.truetype('arial.ttf',20)
 
 w,h=img.size #Obtenemos las dimensiones de la foto para despues redimensionar las medidas obtenidas del wrapper
-
-#An unofficial python wrapper for the Facebook face recognition endpoint
-from fbrecog import FBRecog
     
 # Instantiate the recog class
 recog = FBRecog(TOKEN, COOKIE, FB_DTSG)
@@ -86,37 +85,43 @@ recog = FBRecog(TOKEN, COOKIE, FB_DTSG)
 # Call recognize_raw to get info about the positions of the face for draw an square
 faces = recog.recognize_raw(photo)
 
-# Recorremos todas las caras reconocidas
-for face in faces:
-    #Obtenemos las caracteriticas de cada reconocimiento
-    name=face['recognitions']
+try:
+   # Recorremos todas las caras reconocidas
+   for face in faces:
+       #Obtenemos las caracteriticas de cada reconocimiento
+       name=face['recognitions']       
 
-    if name:
-        #Imprime el nombre de usuario del rostro detectado
-        #print('name: '+name[0]['user']['name'])
+       #La foto que se sube a fb tiene un tamaño de 100x100 (miniatura), por lo tanto,
+       #las coordenadas del centro de los rostros detectados estan de acuerdo a esa resolucion.
+       #La foto original tiene una medida variable, las coordenadas originales no encajarian con 
+       #las obtenidas de FB, para ajustar esto se propone
+       #una sencilla regla de 3 para reescalar las coordenadas.
+
+       #Se calcula la nueva posicion de la coordenada x con la medida del largo de la imagen original (w)
+       posx=face['x']*w/100
+
+       #Se calcula la nueva posicion de la coordenada y con la medida del alto de la imagen original (h)
+       posy=face['y']*h/100
+
+       if name:      
+           #Imprime el nombre de usuario del rostro detectado
+           print('name: '+name[0]['user']['name'])                  
+
+           #Dibujamos sobre la imagen un texto con el nombre de usuario
+           #draw.text((coordenada_en_x,coordenada_en_y),texto,fill=color_del_texto,font=fuente_de_la_letra)
+           draw.text((posx,posy),name[0]['user']['name'],fill='blue',font=font)
+
+           #Para dibujar un rectangulo sobre el rostro tambien hay que escalar las medidas del alto y ancho del rostro detectado
+           #(face['width']*w/100) y (face['height']*h/100)
         
-        #La foto que se sube a fb tiene un tamaño de 100x100 (miniatura), por lo tanto,
-        #las coordenadas del centro de los rostros detectados estan de acuerdo a esa resolucion.
-        #La foto original tiene una medida variable, las coordenadas originales no encajarian con 
-        #las obtenidas de FB, para ajustar esto se propone
-        #una sencilla regla de 3 para reescalar las coordenadas.
-
-        #Se calcula la nueva posicion de la coordenada x con la medida del largo de la imagen original (w)
-        posx=face['x']*w/100
-
-        #Se calcula la nueva posicion de la coordenada y con la medida del alto de la imagen original (h)
-        posy=face['y']*h/100
-
-        #Dibujamos sobre la imagen un texto con el nombre de usuario
-        #draw.text((coordenada_en_x,coordenada_en_y),texto,fill=color_del_texto,font=fuente_de_la_letra)
-        draw.text((posx,posy),name[0]['user']['name'],fill='blue',font=font)
-
-        #Para dibujar un rectangulo sobre el rostro tambien hay que escalar las medidas del alto y ancho del rostro detectado
-        #(face['width']*w/100) y (face['height']*h/100)
-        
-        #Dibujamos un rectangulo sobre el rostro        
-        #draw.rectangle(((posx+(face['width']*w/100)/2,posy+(face['height']*h/100)/2),(posx-(face['width']*w/100)/2,posy-(face['height']*w/100)/2)),fill=None,outline='red')
-    else:
-         draw.text((0,0),"No se encontraron rostros conocidos",fill='blue',font=40)
+           #Dibujamos un rectangulo sobre el rostro        
+           #draw.rectangle(((posx+(face['width']*w/100)/2,posy+(face['height']*h/100)/2),(posx-(face['width']*w/100)/2,posy-(face['height']*w/100)/2)),fill=None,outline='red')
+       else:
+           draw.text((posx,posy),"usuario no reconocido",fill='blue',font=font)
+          
+except AttributeError:
+   print("verifique su coneccion a internet")
+except:
+   print("error inesperado, reintente")
 
 img.save('save.jpg')
